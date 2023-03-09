fdf2e9e71949b35c3accc5a60850fb2e7d08f048
KanchanaRangcharoen <nut.1357@gmail.com>
2023-03-09 14:46:43 +0700

    update2

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find the name of an Asian country.</title>
 </head>
+
 <body>
-<?php
+    <?php
     $Asia = array(
         "Kazakhstan", "Kyrgyzstan", "Tajikistan", "Turkmenistan",
         "Uzbekistan", "China", "China, Hong Kong Special Administrative", "Region", "China, Macao Special Administrative",
@@ -19,23 +21,24 @@
         "State of Palestine", "Syrian Arab Republic", "Turkey", "United Arab Emirates", "Yemen"
     );
 
-    $file= array("hw-doc01.txt","hw-doc02.txt","hw-doc03.txt");
+    $file = array("hw-doc01.txt", "hw-doc02.txt", "hw-doc03.txt");
     $wordf = '';
-    for ($i = 0;$i <=2;$i++) {
-        $wordf = file_get_contents($file[$i]);  
-    $as = [];	//อาร์เรย์ว่างสำหรับเก็บประเทศในAsia
-    foreach ($Asia as $a) {
-          if (stripos($wordf, $a) != null) {     //ถ้ามีประเทศในสตริง
+    for ($i = 0; $i <= 2; $i++) {
+        $wordf = file_get_contents($file[$i]);
+        $as = [];    //อาร์เรย์ว่างสำหรับเก็บประเทศในAsia
+        foreach ($Asia as $a) {
+            if (stripos($wordf, $a) != null) {     //ถ้ามีประเทศในสตริง
                 $as[] = $a;                                //ถ้าพบประเทศในAsiaให้เก็บไว้ในอาร์เรย์
-          }
+            }
+        }
+        $c = array_unique($as);
+        sort($c);
+        if (count($c) > 0) {
+            $s = implode(',', $c);
+            echo "รายชื่อประเทศที่อยู่โซนเอเซีย: <br>$s<br>";
+        }
     }
-    $c = array_unique($as);
-    sort($c);
-    if (count($c) > 0) {
-      $s = implode(',', $c);
-      echo "รายชื่อประเทศที่อยู่โซนเอเซีย: <br>$s<br>";
-      }  
- }
-?>
+    ?>
 </body>
+
 </html>
